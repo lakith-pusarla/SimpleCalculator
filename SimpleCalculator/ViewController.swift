a49ab73dd1da37e8b50bdb679c495cbd5316cc37
lakith-pusarla <38044490+lakith-pusarla@users.noreply.github.com>
2022-04-27 15:24:26 +0530

    Initial UI and layout design completed

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -13,8 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayLabel.text = "0"
     }
 
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.tag)
+        if sender.tag == 15{
+            displayLabel.text = "0"
+        } else {
+            displayLabel.text = String(sender.tag)
+        }
+        
+        
+    }
+    
+    @IBAction func noResponse(_ sender: UIButton){
+        displayLabel.text = "Currently not working"
+    }
+    
+    func changeLabel(_ text: Int){
+        displayLabel.text = String(text)
+    }
+    
 }
 
