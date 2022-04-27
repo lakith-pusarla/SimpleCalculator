78be0da7af62c9acbed5e3146294135ad45104b5
lakith-pusarla <38044490+lakith-pusarla@users.noreply.github.com>
2022-04-27 12:48:06 +0530

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Lakith Pusarla on 27/04/22.
+//  Copyright © 2022 Lakith Pusarla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
